68d1e8ceb8d604206f4365dc827dc7e1faed1fe3
MVBL112004 <longbao112004@gmail.com>
2024-09-05 08:33:56 +0700

    first commit

--- noidung.py
+++ noidung.py
@@ -1,15 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+
 response = requests.get('https://vnexpress.net/23-trieu-hoc-sinh-khai-giang-nam-hoc-moi-4788964.html')
 
 if response.status_code == 200:
-    soup = BeautifulSoup(response.content,'html.parser')
+    soup = BeautifulSoup(response.content, 'html.parser')
     article_tag = soup.find('article')
+    date_tag = soup.find('span', class_='date')
     if article_tag:
-        article_content = article_tag.get_text(strip=True)
+        title = soup.title.string
+        title_content = title.get_text(strip=True)
+        date = date_tag.get_text(strip=True)
+        # Chuyển tiêu đề thành chữ in hoa
+        title_content_upper = title_content.upper()
+        # Thêm tiêu đề vào tệp article_content.txt
         with open('article_content.txt', 'w', encoding='utf-8') as file:
-            file.write(article_content)
+            file.write(title_content_upper + '\n')  # Ghi tiêu đề và xuống dòng
+            file.write(f"Ngày đăng bài: {date}\n")  # Ngày tháng
+            file.write(article_tag.get_text(strip=True)) # Nội dung
+        print("Đã ghi tiêu đề vào tệp article_content.txt thành công!")
     else:
-        print("No <article> tag found.")
+        print("Không tìm thấy thẻ <article> trên trang web.")
 else:
-    print(f"Failed: {response.status_code} ")
+    print(f"Không thể kết nối đến trang web (HTTP status code: {response.status_code}).")
